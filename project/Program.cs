f2da7cfd682d778f40299acba56acc2bc2a1f0d3
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 04:49:58 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,31 @@
+﻿// Создайте коллекцию (массив) с 10 случайными числами.
+// Выведите в консоль номер минимального элемента.
+
+using System;
+class work_1_1
+{
+  static void Main()
+  {
+    int[] numbers = new int[10]; 
+    Random rand = new Random(); 
+    
+    Console.Write("Массив: "); 
+    for (int i = 0; i < 10; i++)
+    {
+        numbers[i] = rand.Next(100); 
+    }
+    var str = string.Join(" ", numbers);
+    Console.WriteLine(str);
+    
+    int min = 100; 
+    for (int i = 0; i < 10; i++)
+    {
+        if (numbers[i] < min) 
+        {
+            min = numbers[i]; 
+        }
+    }
+    Console.WriteLine($"минимальный элемент: {min}"); 
+  }
+}
+
